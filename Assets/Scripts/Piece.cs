37d2100c1db63619e9beeb4832fc14db48e7940e
RyanFensom <ryan.fensom3@tafensw.net.au>
2017-05-10 12:12:26 +1000

    it's fucked

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Piece : MonoBehaviour
+{
+    public int gridX, gridZ;
 }
